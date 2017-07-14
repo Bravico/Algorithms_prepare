1378ceae9490fb19688c7ca57506f93102210ed0
Bravico <bravicohc@gmail.com>
2017-07-15 00:43:01 +0800

    标准答案中，对i和j的控制更加准确，如果重复，i++,如果不重复，j++。这个加深了我对slide window的理解

--- algorithms_preparing/src/leetcode/Solution3.java
+++ algorithms_preparing/src/leetcode/Solution3.java
@@ -7,6 +7,8 @@ package leetcode;
  * Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
  */
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Solution3 {
 	public int lengthOfLongestSubstring(String s) {
@@ -40,4 +42,17 @@ public class Solution3 {
     //     }
     //     return false;
     // }
+	 public int lengthOfLongestSubstring2(String s) {
+	        int n = s.length(), ans = 0;
+	        Map<Character, Integer> map = new HashMap<>(); // current index of character
+	        // try to extend the range [i, j]
+	        for (int j = 0, i = 0; j < n; j++) {
+	            if (map.containsKey(s.charAt(j))) {
+	                i = Math.max(map.get(s.charAt(j)), i);
+	            }
+	            ans = Math.max(ans, j - i + 1);
+	            map.put(s.charAt(j), j + 1);
+	        }
+	        return ans;
+	    }
 }
