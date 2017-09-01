f206e3e313b92a1a402c6e004404433d17210bb2
Bravico <bravicohc@gmail.com>
2017-09-01 11:17:46 +0800

    upload the selection class file, and it can be re-factored as an API

---
+++ algorithms_preparing/src/sort/Selection.java
@@ -0,0 +1,30 @@
+package sort;
+
+public class Selection {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer[] arr = {126,272,8,165,123,12,28}; //default value
+		Shell.sort(arr);
+		for(int i=0;i<arr.length;i++)
+			System.out.println(arr[i]);
+	
+	}
+	
+	public static void sort(Comparable[] a){
+		int N = a.length;
+		for(int i=0;i<N;i++){
+			int min = i;
+			for(int j=i+1; j<N; j++){
+				if(a[i].compareTo(a[j])>0) min = j;
+			}
+			swap(a, i, min);
+		}
+	}
+	
+	private static void swap(Comparable[] arr,int x, int y) {
+		Comparable temp = arr[x];
+		arr[x] = arr[y];
+		arr[y] = temp;
+	}
+}
