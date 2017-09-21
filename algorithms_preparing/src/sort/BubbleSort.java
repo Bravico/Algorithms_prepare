e579b6be7be5d9d5ee51066941ffcd0b42fd2eea
Bravico <bravicohc@gmail.com>
2017-09-21 17:33:24 +0800

    update the bubbleSort method to Comparable arrays

--- algorithms_preparing/src/sort/BubbleSort.java
+++ algorithms_preparing/src/sort/BubbleSort.java
@@ -1,28 +1,27 @@
 package sort;
 
 public class BubbleSort {
-	int[] arr = { 126, 272, 8, 165, 123, 12, 28 }; // default value
-
+	
 	public static void main(String[] args) {
+		Comparable[] arr = { 126, 272, 8, 165, 123, 12, 28 }; // default value
 		BubbleSort bs = new BubbleSort();
-		bs.bubblesort(bs.arr, bs.arr.length);
-		for(int i=0;i<bs.arr.length;i++)
-			System.out.println(bs.arr[i]);
+		BubbleSort.sort(arr, arr.length);
+		for(int i=0;i<arr.length;i++)
+			System.out.println(arr[i]);
 	}
 
 	// 从小到大
-	public void bubblesort(int[] arr, int length) {
+	public static void sort(Comparable[] arr, int length) {
 		for (int i = 0; i < length; i++)
 			for (int j = 0; j < length - 1 - i; j++) {
-//			for (int j = i+1; j < length; j++) {
-				if (arr[j] > arr[j + 1]) {
-					swap(j, j + 1);
+				if (arr[j].compareTo(arr[j + 1])>0) {
+					swap(arr,j, j + 1);
 				}
 			}
 	}
 
-	private void swap(int x, int y) {
-		int temp = arr[x];
+	private static void swap(Comparable[] arr, int x, int y) {
+		Comparable temp = arr[x];
 		arr[x] = arr[y];
 		arr[y] = temp;
 	}
